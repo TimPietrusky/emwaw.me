e332c9748ab3be558496885884864e15bd2c9712
Tim Pietrusky <timpietrusky@googlemail.com>
2013-07-21 21:11:02 +0200

    Added emwawmesess! ┌∩┐(◣_◢)┌∩┐

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php    $names = array(
+	'alpha',
+	'beta',
+	'gamma',
+	'delta',
+	'epsilon',
+	'zeta',
+	'eta',
+	'theta',
+	'iota',
+	'kappa',
+	'lambda',
+	'my',
+	'ny',
+	'xi',
+	'omikron',
+	'pi'
+); 
+
+print_r($names); ?>
